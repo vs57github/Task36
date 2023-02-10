3d424705592127041721af9ac0b40cd8b86d62c9
vs57github <123260473+vs57github@users.noreply.github.com>
2023-02-10 15:06:01 +0300

    Add files via upload

---
+++ Task36.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный 
+случайными числами. Найдите сумму элементов, стоящих
+ на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
+int[] array = new int[4];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите число: ");  
+    array[i] = int.Parse(Console.ReadLine());  
+}
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        sum += array[i];
+    }
+}
+Console.WriteLine(sum); 
